0ab1067b2138d55bb1cfab46d4a9293be26fb106
tbsteeze <trevbrow@live.com>
2019-08-21 21:18:51 -0600

    added available google maps in order to plot multiple items on your map

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,15 +7,46 @@ var firebaseConfig = {
     messagingSenderId: "1013960643985",
     appId: "1:1013960643985:web:d0b61bc128518812"
 };
+
+
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 var database = firebase.database();
 
 
+
 var map;
+
 function initMap() {
+  //Below is all sample data representing potential JSON returns from freelancers
+  var SLC = {lat: 40.7608, lng: -111.8910};
   map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 40.768, lng: -111.891},
-    zoom: 13
+    center: SLC,
+    zoom: 11
   });
+  var seekerReturn = [
+    jobseeker1 = {
+      name : 'John Smith',
+      skills: ['JSON', 'HTML', 'React', 'bullshitting'],
+      location: {lat: 40.7508, lng: -111.8610}
+    },
+    jobseeker2 = {
+      name : 'Hane Doe',
+      skills : ['jQuery', 'bootstrap', 'React', 'bullshitting'],
+      location : {lat: 40.7421, lng: -111.9110}
+    },
+    jobseeker3 = {
+      name : 'Jane Donte',
+      skills : ['jQuery', 'bootstrap', 'React', 'bullshitting'],
+      location : {lat: 40.8708, lng: -111.7910}
+    }
+  ]
+  var marker = new google.maps.Marker({position: SLC, map: map});
+  var marker = new google.maps.Marker({position: seekerReturn[0].location, map: map});
+  var marker = new google.maps.Marker({position: seekerReturn[1].location, map: map});
+  var marker = new google.maps.Marker({position: seekerReturn[2].location, map: map});
+  console.log(seekerReturn[0].location);
+  
+
+ 
 }
