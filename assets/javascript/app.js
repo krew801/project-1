0c5db88bbec1f0f824e0a286e5d3d3394a17152c
savior af <qushairia1@gmail.com>
2019-08-24 09:45:52 -0600

    try1

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -18,35 +18,80 @@ var database = firebase.database();
 var map;
 
 function initMap() {
-  //Below is all sample data representing potential JSON returns from freelancers
-  var SLC = {lat: 40.7608, lng: -111.8910};
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: SLC,
-    zoom: 11
-  });
-  var seekerReturn = [
-    jobseeker1 = {
-      name : 'John Smith',
-      skills: ['JSON', 'HTML', 'React', 'bullshitting'],
-      location: {lat: 40.7508, lng: -111.8610}
-    },
-    jobseeker2 = {
-      name : 'Hane Doe',
-      skills : ['jQuery', 'bootstrap', 'React', 'bullshitting'],
-      location : {lat: 40.7421, lng: -111.9110}
-    },
-    jobseeker3 = {
-      name : 'Jane Donte',
-      skills : ['jQuery', 'bootstrap', 'React', 'bullshitting'],
-      location : {lat: 40.8708, lng: -111.7910}
-    }
-  ]
-  var marker = new google.maps.Marker({position: SLC, map: map});
-  var marker = new google.maps.Marker({position: seekerReturn[0].location, map: map});
-  var marker = new google.maps.Marker({position: seekerReturn[1].location, map: map});
-  var marker = new google.maps.Marker({position: seekerReturn[2].location, map: map});
-  console.log(seekerReturn[0].location);
-  
+    //Below is all sample data representing potential JSON returns from freelancers
+    var SLC = { lat: 40.7608, lng: -111.8910 };
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: SLC,
+        zoom: 11
+    });
+    var seekerReturn = [
+        jobseeker1 = {
+            name: 'John Smith',
+            skills: ['JSON', 'HTML', 'React', 'bullshitting'],
+            location: { lat: 40.7508, lng: -111.8610 }
+        },
+        jobseeker2 = {
+            name: 'Hane Doe',
+            skills: ['jQuery', 'bootstrap', 'React', 'bullshitting'],
+            location: { lat: 40.7421, lng: -111.9110 }
+        },
+        jobseeker3 = {
+            name: 'Jane Donte',
+            skills: ['jQuery', 'bootstrap', 'React', 'bullshitting'],
+            location: { lat: 40.8708, lng: -111.7910 }
+        }
+    ]
+    var marker = new google.maps.Marker({ position: SLC, map: map });
+    var marker = new google.maps.Marker({ position: seekerReturn[0].location, map: map });
+    var marker = new google.maps.Marker({ position: seekerReturn[1].location, map: map });
+    var marker = new google.maps.Marker({ position: seekerReturn[2].location, map: map });
+    console.log(seekerReturn[0].location);
 
- 
+
+
 }
+map = new google.maps.Map(document.getElementById('map'), {
+    center: { lat: 40.768, lng: -111.891 },
+    zoom: 13
+});
+}
+
+function renderSearch(searchList) {
+    $("#searchDiv").empty();
+
+    for (let i = 0; i < searchList.length; i++) {
+
+        var searchItem = $("<p>")
+        searchItem.text(searchList[i]);
+
+        $("#searchDiv").append(searchItem);
+    }
+}
+console.log(searchList);
+
+$("#add-search").on("click", function(event) {
+    event.preventDefault();
+    console.log("#add-search");
+
+    var toLocalList = $("#text-box").val().trim();
+    console.log(toLocalList);
+
+    searchList.push(toLocalList);
+    console.log(searchList);
+
+    renderSearch(searchList);
+
+
+    localStorage.setItem("tosearchList", JSON.stringify(searchList));
+
+    $("#text-box").val("");
+});
+
+
+var searchList = JSON.parse(localStorage.getItem("tosearchList"));
+
+if (!Array.isArray(searchList)) {
+    searchList = [];
+}
+
+renderSearch(searchList);
