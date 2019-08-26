f9941958807708c92dc81ac2e125db9b3ba0a0ea
Rob Canfield <git@livesenjoyable.com>
2019-08-26 15:17:25 -0600

    Job cards are now displaying nicely thanks to Trevor's code. I added two more buttons,in addition to the apply button: locate company (link to map API), and show/hide details.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -21,22 +21,30 @@ $('.search-btn').on('click', function(){
 
   }).then(function(r) {
 
-      // var searchDisplay = $("#")
+   
+      
 
-      $(".search-title").html("<h4>showing results for " + language + " jobs in " + location + ".</h4>");
+      
+    
+
+     
     
     
       //For each job in the response, create a new div dynamically to display the data
       for (let i = 0; i < r.length; i++) {
+
+        $(".search-title").html("<h4>showing " + r.length + " results for " + language + " jobs in " + location + ".</h4>");
+
+
     // Creates div to store basic company search result information as a bootstrap card
         
-    
+        
     
     
     // Created second div to store information within the card body
         var cardBody = $('<div>').addClass('card-body');
         var cardBodyRow = $('<div>').addClass('row body-cont');
-        var compImg = $('<div>').addClass('col-sm-2 comp-img');
+        var compImg = $('<div>').addClass('col-sm-4');
         var company_logo = $('<div>').addClass('float-left');
         var companyLogoImg = $('<img>').attr('src', r[i].company_logo);
         
@@ -56,7 +64,7 @@ $('.search-btn').on('click', function(){
        
        
         //This div contains all the body information for the card other than the company logo
-        var cardBodyDiv = $('<div>').addClass('col-4');
+        var cardBodyDiv = $('<div>').addClass('col-sm-8');
         
         
         
@@ -69,27 +77,34 @@ $('.search-btn').on('click', function(){
         
         
         //Create div for company and add h3 holding the company name to it
-        var compDiv = $('<div>').attr('id','company').append($('<h3>').text(r[i].company))
+        var compDiv = $('<div>').attr('id','company').append($('<h3 id="job-title">').text(r[i].company))
         
         
         
         
         //Create div for company site and append link to it
         var companySite = $('<div>').attr('id', 'company_url').append($('<a>').text(r[i].company_url).attr('href', r[i].company_url).addClass('card-text'));
-        var applyNow = $('<div>').append($('<a>').attr('href', r[i].how_to_apply).text('Apply Now').addClass('btn btn-primary'));//Change color of this button to better reflect styling on results page
+        
+        
+        
+        
+        var applyNow = $('<div class="listing-btn">').append($('<a>').attr('href', r[i].how_to_apply).text('apply now').addClass('btn btn-primary'));//Change color of this button to better reflect styling on results page
+        var locateCompany = $('<div class="listing-btn">').append($('<a>').attr('href', r[i].how_to_apply).text('locate company').addClass('btn btn-primary'));//Change color of this button to better reflect styling on results page
+        var viewDetails = $('<div class="listing-btn">').text('view job details').addClass('btn btn-primary');//Change color of this button to better reflect styling on results page
+
         
         
         
         
         //Add info to the div within card body
-        cardBodyDiv.append(titleDiv, compDiv, companySite, applyNow);
+        cardBodyDiv.append(titleDiv, compDiv, companySite, applyNow, locateCompany, viewDetails);
        
        
        
        
         // Creating elements to store information in card, with corresponding classes to format in bootstrap
-        var desc = $('<div>').append(r[i].description);
-        desc.addClass('col-6 float-right');
+        var desc = $('<div>').attr('id', "description").addClass("row body-cont").append(r[i].description);
+        
         
         
         
@@ -120,25 +135,25 @@ $('.search-btn').on('click', function(){
 
 
 
-      var map;
+    //   var map;
 
-    function initMap() {
-      var SF = {lat: 37.7749, lng: -122.4194};
-      map = new google.maps.Map(document.getElementById('google-maps'), {
-        center: SF,
-        zoom: 10
-      });
+    // function initMap() {
+    //   var SF = {lat: 37.7749, lng: -122.4194};
+    //   map = new google.maps.Map(document.getElementById('google-maps'), {
+    //     center: SF,
+    //     zoom: 10
+    //   });
 
-      var marker = new google.maps.Marker({position: SF, map: map});
+    //   var marker = new google.maps.Marker({position: SF, map: map});
 
       
 
     
-    }
+    // }
 
 
 
-    initMap();
+    // initMap();
 })
 
 // if (searched === true){
